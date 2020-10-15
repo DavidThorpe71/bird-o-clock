33410096c496654052587d0ac9ffbd579f1c9a4e
David Thorpe <davidthorpe878@gmail.com>
2020-10-15 21:23:00 +0100

    Upload to s3

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const axios = require('axios').default;
+const AWS = require('aws-sdk');
+
 // This will be a Lambda at some point
 
 // Get random bird call
@@ -10,15 +12,36 @@ function getRandomNumber(max) {
 }
 
 async function getBird() {
-  const endpoint = `https://www.xeno-canto.org/api/2/recordings?query=q:A+len_lt:15`
+  const initalEndpoint = `https://www.xeno-canto.org/api/2/recordings?query=q:A+len_lt:15`
 
   try {
-    const response = await axios.get(endpoint);
+    const response = await axios.get(initalEndpoint);
     console.log("number of pages: ", response.data.numPages);
-    const randomPage = getRandomNumber(response.data.numPage)
-    console.log("number of recordings: ", response.data.numRecordings);
-    console.log("length of recordings: ", response.data.recordings.length);
-    // console.log(response.data.numPages);
+    const randomPage = getRandomNumber(response.data.numPages);
+    const randomNumber = getRandomNumber(500);
+
+    const secondaryEndpoint = `https://www.xeno-canto.org/api/2/recordings?query=q:A+len_lt:15&page=${randomPage}`;
+    const secondList = await axios.get(secondaryEndpoint);
+
+    const item = secondList.data.recordings[randomNumber];
+
+    const s3 = new AWS.S3();
+
+    axios({
+      method: 'get',
+      url: `http:${item.file}`,
+      responseType: 'stream'
+    })
+    .then(function (response) {
+      var params = {Bucket: `bird-o-clock`, Key: item["file-name"], Body: response.data};
+      s3.upload(params, function(err, data) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log("Successfully uploaded data to " + `bird-o-clock` + "/" + item["file-name"]);
+        }
+      });
+    });
   } catch (error) {
     console.error(error);
   }
